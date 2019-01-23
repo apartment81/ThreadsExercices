c676b66cb611f633b40abec53d371f36f8300f55
apartment81 <bogdan.mirodone@gmail.com>
2019-01-23 19:48:05 +0200

    commit 1 - 23012019

---
+++ src/ro/mirodone/ThreadColor.java
@@ -0,0 +1,11 @@
+package ro.mirodone;
+
+public class ThreadColor {
+
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+}
